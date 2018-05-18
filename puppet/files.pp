4d0f82b3d0c5f577edd0e84513af7349b129d4ec
Adam Buxton <buxton.adam@gmail.com>
2018-05-18 09:46:15 +0100

    update to add puppet code, needs refactor

---
+++ puppet/files.pp
@@ -0,0 +1,80 @@
+file { '/Users/abuxton/.atom':
+  ensure => 'link',
+  ctime  => '2018-04-10 15:51:54 +0100',
+  group  => 20,
+  mode   => '0755',
+  mtime  => '2018-04-10 15:51:54 +0100',
+  owner  => 502,
+  target => '/Users/abuxton/Dropbox/profile/dotfiles/atom',
+  type   => 'link',
+}
+file { '/Users/abuxton/.bash_profile':
+  ensure => 'link',
+  ctime  => '2018-03-08 12:33:55 +0000',
+  group  => 20,
+  mode   => '0755',
+  mtime  => '2018-03-08 12:33:55 +0000',
+  owner  => 502,
+  target => '/Users/abuxton/Dropbox/profile/dotfiles/bash_profile',
+  type   => 'link',
+}
+file { '/Users/abuxton/.bashrc':
+  ensure => 'link',
+  ctime  => '2018-03-08 12:39:57 +0000',
+  group  => 20,
+  mode   => '0755',
+  mtime  => '2018-03-08 12:39:57 +0000',
+  owner  => 502,
+  target => '/Users/abuxton/Dropbox/profile/dotfiles/bashrc',
+  type   => 'link',
+}
+file { '/Users/abuxton/.eyaml':
+  ensure => 'link',
+  ctime  => '2018-05-08 13:26:18 +0100',
+  group  => 20,
+  mode   => '0755',
+  mtime  => '2018-05-08 13:26:18 +0100',
+  owner  => 502,
+  target => '/Users/abuxton/Dropbox/profile/dotfiles/eyaml',
+  type   => 'link',
+}
+file { '/Users/abuxton/.gitconfig':
+  ensure => 'link',
+  ctime  => '2018-03-06 12:03:48 +0000',
+  group  => 20,
+  mode   => '0755',
+  mtime  => '2018-03-06 12:03:48 +0000',
+  owner  => 502,
+  target => '/Users/abuxton/Dropbox/profile/dotfiles/bck.gitconfig',
+  type   => 'link',
+}
+file { '/Users/abuxton/.puppetforge.yml':
+  ensure => 'link',
+  ctime  => '2018-03-20 12:36:17 +0000',
+  group  => 20,
+  mode   => '0755',
+  mtime  => '2018-03-20 12:36:17 +0000',
+  owner  => 502,
+  target => '/Users/abuxton/Dropbox/profile/dotfiles/puppetforge.yml',
+  type   => 'link',
+}
+file { '/Users/abuxton/.ssh':
+  ensure => 'link',
+  ctime  => '2018-03-06 11:42:04 +0000',
+  group  => 20,
+  mode   => '0755',
+  mtime  => '2018-03-06 11:42:04 +0000',
+  owner  => 502,
+  target => '/Users/abuxton/Dropbox/profile/dotfiles/ssh',
+  type   => 'link',
+}
+file { '/Users/abuxton/.tmux.conf':
+  ensure => 'link',
+  ctime  => '2018-03-14 13:46:07 +0000',
+  group  => 20,
+  mode   => '0755',
+  mtime  => '2018-03-14 13:46:07 +0000',
+  owner  => 502,
+  target => '/Users/abuxton/.dotfiles/tmux.conf',
+  type   => 'link',
+}
