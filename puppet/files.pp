fd3bdd511777741958a7f9601707767f38d33a25
Adam Buxton <buxton.adam@gmail.com>
2018-05-18 14:07:05 +0100

    files resource

--- puppet/files.pp
+++ puppet/files.pp
@@ -1,80 +1,50 @@
-file { '/Users/abuxton/.atom':
-  ensure => 'link',
-  ctime  => '2018-04-10 15:51:54 +0100',
-  group  => 20,
-  mode   => '0755',
-  mtime  => '2018-04-10 15:51:54 +0100',
-  owner  => 502,
-  target => '/Users/abuxton/Dropbox/profile/dotfiles/atom',
-  type   => 'link',
-}
-file { '/Users/abuxton/.bash_profile':
-  ensure => 'link',
-  ctime  => '2018-03-08 12:33:55 +0000',
-  group  => 20,
-  mode   => '0755',
-  mtime  => '2018-03-08 12:33:55 +0000',
-  owner  => 502,
-  target => '/Users/abuxton/Dropbox/profile/dotfiles/bash_profile',
-  type   => 'link',
-}
-file { '/Users/abuxton/.bashrc':
-  ensure => 'link',
-  ctime  => '2018-03-08 12:39:57 +0000',
-  group  => 20,
-  mode   => '0755',
-  mtime  => '2018-03-08 12:39:57 +0000',
-  owner  => 502,
-  target => '/Users/abuxton/Dropbox/profile/dotfiles/bashrc',
-  type   => 'link',
-}
-file { '/Users/abuxton/.eyaml':
-  ensure => 'link',
-  ctime  => '2018-05-08 13:26:18 +0100',
-  group  => 20,
-  mode   => '0755',
-  mtime  => '2018-05-08 13:26:18 +0100',
-  owner  => 502,
-  target => '/Users/abuxton/Dropbox/profile/dotfiles/eyaml',
-  type   => 'link',
-}
-file { '/Users/abuxton/.gitconfig':
-  ensure => 'link',
-  ctime  => '2018-03-06 12:03:48 +0000',
-  group  => 20,
-  mode   => '0755',
-  mtime  => '2018-03-06 12:03:48 +0000',
-  owner  => 502,
-  target => '/Users/abuxton/Dropbox/profile/dotfiles/bck.gitconfig',
-  type   => 'link',
-}
-file { '/Users/abuxton/.puppetforge.yml':
-  ensure => 'link',
-  ctime  => '2018-03-20 12:36:17 +0000',
-  group  => 20,
-  mode   => '0755',
-  mtime  => '2018-03-20 12:36:17 +0000',
-  owner  => 502,
-  target => '/Users/abuxton/Dropbox/profile/dotfiles/puppetforge.yml',
-  type   => 'link',
-}
-file { '/Users/abuxton/.ssh':
-  ensure => 'link',
-  ctime  => '2018-03-06 11:42:04 +0000',
-  group  => 20,
-  mode   => '0755',
-  mtime  => '2018-03-06 11:42:04 +0000',
-  owner  => 502,
-  target => '/Users/abuxton/Dropbox/profile/dotfiles/ssh',
-  type   => 'link',
-}
-file { '/Users/abuxton/.tmux.conf':
-  ensure => 'link',
-  ctime  => '2018-03-14 13:46:07 +0000',
-  group  => 20,
-  mode   => '0755',
-  mtime  => '2018-03-14 13:46:07 +0000',
-  owner  => 502,
-  target => '/Users/abuxton/.dotfiles/tmux.conf',
-  type   => 'link',
+
+  file { '/Users/abuxton/.atom':
+    ensure => 'link',
+    mode   => '0755',
+    target => '/Users/abuxton/Dropbox/profile/dotfiles/atom',
+    force => true,
+    #type   => 'link',
+  }
+  file { '/Users/abuxton/.bash_profile':
+    ensure => 'link',
+    mode   => '0755',
+    target => '/Users/abuxton/Dropbox/profile/dotfiles/bash_profile',
+    #type   => 'link',
+  }
+  file { '/Users/abuxton/.bashrc':
+    ensure => 'link',
+    mode   => '0755',
+    target => '/Users/abuxton/Dropbox/profile/dotfiles/bashrc',
+  }
+  file { '/Users/abuxton/.eyaml':
+    ensure => 'link',
+    mode   => '0755',
+    target => '/Users/abuxton/Dropbox/profile/dotfiles/eyaml',
+  }
+  file { '/Users/abuxton/.gitconfig':
+    ensure => 'link',
+    mode   => '0755',
+    target => '/Users/abuxton/Dropbox/profile/dotfiles/bck.gitconfig',
+  }
+  file { '/Users/abuxton/.puppetforge.yml':
+    ensure => 'link',
+    mode   => '0755',
+    target => '/Users/abuxton/Dropbox/profile/dotfiles/puppetforge.yml',
+  }
+  file { '/Users/abuxton/.ssh':
+    ensure => 'link',
+    mode   => '0755',
+    target => '/Users/abuxton/Dropbox/profile/dotfiles/ssh',
+  }
+  file { '/Users/abuxton/.tmux.conf':
+    ensure => 'link',
+    mode   => '0755',
+    target => '/Users/abuxton/.dotfiles/tmux.conf',
+  }
+file { '/Users/abuxton/.profile':
+  ensure  => 'file',
+  content => '{md5}d41d8cd98f00b204e9800998ecf8427e',
+  mode    => '0644',
+  #type    => 'file',
 }
